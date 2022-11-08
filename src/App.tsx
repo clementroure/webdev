11f68010e8e86946955cbe3d8a0ae9d7c59f3144
Clement549 <clementroure@orange.fr>
2022-11-08 18:32:31 +0100

    Initialisation React app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      
+         <h1 className='grid h-screen place-items-center text-xl'>Salut</h1>
+
+    </div>
+  );
+}
+
+export default App;
